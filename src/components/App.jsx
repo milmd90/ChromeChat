4d6719029e64f490ee5d9cd08ba52f80d9439929
Matthew Miller <mattdeanmiller@gmail.com>
2017-05-05 21:04:19 -0500

    Accidentally added js bundle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import request from 'request';
 
 class App extends Component {
+    create() {
+        var options = {
+            method: 'GET',
+            url: 'https://chromechat-3fe8.restdb.io/rest/messages',
+            headers:
+            {   'cache-control': 'no-cache',
+                'x-apikey': '590d2e342040bc250c45d89e' 
+            }
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            console.log(body);
+        });
+    }
     render() {
         return (
             <div>Hello Chrome!</div>
